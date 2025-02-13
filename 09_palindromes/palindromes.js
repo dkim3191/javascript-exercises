a6f9ae24de856c3d8412996b40728bb544638e8a
dkim3191 <dkim3191@gmail.com>
2025-02-13 14:57:07 -0500

    09 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    string = string.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+    string = string.split(" ").join("");
+    len = string.length;
+    for (let i = 0; i < Math.trunc(len/2); i++) {
+        if (string.charAt(i) == string.charAt(len-1-i)) {
+            continue;
+        } else {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
