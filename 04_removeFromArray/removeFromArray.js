22651dd47b3a242b66bfb523232884dc53c66ec8
dkim3191 <dkim3191@gmail.com>
2025-02-13 13:55:18 -0500

    changes up to act 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...num) {
+    let values = Array.from(num);
+
+    for (let i = array.length-1; i >= 0; i--) {
+        for (let j = 0; j < values.length; j++) {
+            if (array[i] === values[j]) {
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
