d812a02d3936c6d29cd9a62aa33c0d11f40cf0a8
dkim3191 <dkim3191@gmail.com>
2025-02-18 14:23:23 -0500

    12 complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+    for (let person of arr) {
+        if (person.yearOfDeath == undefined) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+    }
+    let oldest = arr[0];
+    let oldestAge = arr[0].yearOfDeath - arr[0].yearOfBirth;
 
+    for (let person of arr) {
+        if ((person.yearOfDeath - person.yearOfBirth) > oldestAge) {
+            oldestAge = person.yearOfDeath - person.yearOfBirth;
+            oldest = person;
+        }
+    }
+
+    return oldest;
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
